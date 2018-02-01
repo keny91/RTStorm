e8ebd872c87556413e9126205393c7e518030350
Luis Herranz Mañas <luishmanas@gmail.com>
2018-02-01 16:44:47 +0100

    Update GameStructs.cpp

--- sources/InGame/GameStructs.cpp
+++ sources/InGame/GameStructs.cpp
@@ -4,3 +4,34 @@ class Effect
 {
 	
 };
+
+
+// abstract class; use it as the parent model for the rest
+class modifier
+{
+
+	public:
+	//
+	void* reference;   // class/struct that has this mod associated
+	ModifierType type;  // 
+	
+	
+	protected:   // children will have these defined
+	
+	
+	virtual int applyModifier (int value);
+	virtual int modifyValue (int amount);
+	virtual bool reachedCap();
+	virtual void *getParent() = 0;  // pure virtual -> must be redifined 
+	// dpending on the child we know which is our type of parent (type of pointer returning)
+	
+	//private:	// no-sense to declare any privates in the parent
+	
+};
+
+
+class AttackModifier : protected modifier 
+{
+
+}
+
