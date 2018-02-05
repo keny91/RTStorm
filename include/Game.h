aa4ae001a7730cb620dc0310cb4b68406c4f34a9
Luis Herranz Mañas <luishmanas@gmail.com>
2018-02-05 17:35:30 +0100

    Update Game.h

--- include/Game.h
+++ include/Game.h
@@ -18,8 +18,16 @@ Ex: Death, score, ...
 
 extern class Team;
 
-extern class ScoreCoreGame {};
-extern class ScorePointedGame {};
+// all this is defined in score.h so it is not necessary
+extern class ScoreCoreGame;
+extern class ScorePointedGame;
+extern class TeamScore;
+
+
+// all these should be defined in RT.h
+extern Struct RTData;
+extern void RTSetNewGameEventCB(HostData * data); //host data 
+extern void RTSetErrorOnProcessRTDataCB(int error_id, string error_msg); //host data
 
 
 typedef int effecttype;
@@ -30,10 +38,11 @@ class Game
 public: 
 	Game();
 	~Game();
-	
+
+protected:
 	int duration;   // In seconds / time?
-	Score score_team_red;	// reference to score structure
-	Score score_team_blue;
+	TeamScore score_team_red;	// reference to score structure
+	TeamScore score_team_blue;
 	
 	Team team_red;	// Reference to team structure
 	Team team_blue;
@@ -49,15 +58,13 @@ public:
 	// next probably goes in "ENGINE"
 	int SetNextEventCallback(int timing, (void *) funct );  // New thread should trigger an event when the marked time comes.
 	int InitializeKeyPressCallbacks();  // predefined function that compiles a bunch of ON-KEY-PRESS callbacks.  
+	int InitHostEngine();
 	
-	int 
 	// Get/Set functions
 	
 	
 private:
 
-
-protected:
 
 };
 
